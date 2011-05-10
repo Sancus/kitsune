ce008e92138db3d5535123c94f3caae1392d8d45
Andrei Hajdukewycz <sancus@off.net>
2011-05-10 13:29:31 -0700

    remove index removals that we have no need for

--- migrations/98-gallery-is-draft.sql
+++ migrations/98-gallery-is-draft.sql
@@ -12,9 +12,3 @@ ALTER TABLE `gallery_video`
 ALTER TABLE `gallery_image`
     ADD UNIQUE `gallery_image_is_draft_creator_id` (`is_draft`, `creator_id`);
 
--- Drop an unnecessary index on locale
-ALTER TABLE `gallery_video`
-    DROP INDEX `gallery_video_locale`;
-
-ALTER TABLE `gallery_image`
-    DROP INDEX `gallery_image_locale`;
