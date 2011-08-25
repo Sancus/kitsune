b0887e25e3cccaf59c851ceb064e98376cb87fe7
Andrei Hajdukewycz <sancus@off.net>
2011-08-25 11:13:04 -0700

    modified migrations for tb-kitsune

--- migrations/109-user-profile-permissions.sql
+++ migrations/109-user-profile-permissions.sql
@@ -1,3 +1,5 @@
+INSERT IGNORE django_content_type VALUES(0, 'profile', 'users', 'user');
+
 SET @ct = (SELECT id from django_content_type WHERE name='profile' AND app_label='users');
 
 INSERT INTO auth_permission (`name`, `content_type_id`, `codename`) VALUES
