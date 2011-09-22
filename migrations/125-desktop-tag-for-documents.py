75c866af7a5932830d6df05db58c2b870f9db239
Andrei Hajdukewycz <sancus@off.net>
2011-09-22 10:53:11 -0700

    fix desktop migration for thunderbird

--- migrations/125-desktop-tag-for-documents.py
+++ migrations/125-desktop-tag-for-documents.py
@@ -1,5 +1,5 @@
 SET @ct = (SELECT id from django_content_type WHERE name='document' AND app_label='wiki');
-SET @tag = (SELECT id FROM taggit_tag WHERE name = 'desktop');
+SET @tag = (SELECT id FROM taggit_tag WHERE name = 'Thunderbird');
 
 INSERT IGNORE INTO taggit_taggeditem (tag_id, object_id, content_type_id)
     SELECT DISTINCT @tag, document_id, @ct FROM wiki_firefoxversion WHERE item_id IN (1,2,3,5,6,9)
